0930797b67f23bb81ba08205e67991c3f6d3de3e
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-09-03 16:27:25 -0700

    Fully defined array.

--- scripts.js
+++ scripts.js
@@ -1,27 +1,47 @@
-const quizContainer = document.getElementByID('quiz');
+const quizContainer = document.getElementByID("quiz");
 
-const resultsContainer = document.getElementById('results');
+const resultsContainer = document.getElementById("results");
 
-const submitButton = document.getElementById('submit');
+const submitButton = document.getElementById("submit");
 
-function buildQuiz() {
-  
-}
+function buildQuiz() {}
 
 //Display the quiz
 
 buildQuiz();
 
-submitButton.addEventListener('click', showResults);
+submitButton.addEventListener("click", showResults);
 
-const myQuestions =[
-{
+const myQuestions = [
+    //First Question
+  {
     question: "Which animal is a dog?",
     answers: {
-        a: "Rock",
-        b: "Parrot",
-        c: "Table",
+      a: "Rock",
+      b: "Parrot",
+      c: "Table",
     },
-},
 
-]
+    correctAnswer: "b",
+  },
+  //Second Question
+  {
+      question: "Which is a type of bird?",
+      answers: {
+          a: "Tucan",
+          b: "Siberian Husky",
+          c: "Mouse"
+      },
+      correctAnswer: "a",
+  }
+  //Third Question
+  {
+  question: "How do birds teach their young to fly?",
+  answers: {
+    a: "Carry them on their backs",
+    b: "Slow take off from the ground",
+    c: "Yeet them out of the nest"
+  },
+  correctAnswer: "c",
+  }
+];
