e5154e7dd19a11f3a83e597dfdd3e2f068a9b8d2
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-09-03 16:14:47 -0700

    Added function buildQuiz

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const quizContainer = document.getElementByID('quiz');
+
+const resultsContainer = document.getElementById('results');
+
+const submitButton = document.getElementById('submit');
+
+function buildQuiz() {
+  
+}
+
+//Display the quiz
+
+buildQuiz();
