683d967adffddaeeae634d621ba800449ee385a7
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-09-08 11:46:23 -0700

    Questions finally displayed.

--- scripts.js
+++ scripts.js
@@ -1,97 +1,156 @@
-const quizContainer = document.getElementByID("quiz");
-
-const resultsContainer = document.getElementById("results");
-
-const submitButton = document.getElementById("submit");
-
-function buildQuiz() {
-    const output = [];
-    //For loop for each question
-    myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
-        //Variable storing list of possible answers
-        const answers =[];
-
-        //Contains available answers
-        for (letter in currentQuestion.Answers) {
-            //Adds a radio button and utilizes $ to add javascript elements to html
+(function(){
+    // Functions
+    function buildQuiz(){
+      // variable to store the HTML output
+      const output = [];
+  
+      // for each question...
+      myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+  
+          // variable to store the list of possible answers
+          const answers = [];
+  
+          // and for each available answer...
+          for(letter in currentQuestion.answers){
+  
+            // ...add an HTML radio button
             answers.push(
-                `<label>
-                    <input type="radio" name="question${questionNumber}" value="${letter}">
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                </label>`
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
             );
+          }
+  
+          // add this question and its answers to the output
+          output.push(
+            `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
+          );
         }
-
-        output.push(
-            `<div class="question">{$currentQuestion.question</div>
-            <div class="answers"> ${answers.join('')}</div>`
-        );
-
+      );
+  
+      // finally combine our output list into one string of HTML and put it on the page
+      quizContainer.innerHTML = output.join('');
     }
-    );
-    quizContainer.innerHtml = output.join('');
-}
-
-function showResults() {
-    const answerContainers = quizContainer.querySelectorAll('.answers');
-    
-    let numCorrect = 0;
-
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+    function showResults(){
+  
+      // gather answer containers from our quiz
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+      // keep track of user's answers
+      let numCorrect = 0;
+  
+      // for each question...
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+        // find selected answer
         const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumbne}]:checked`;
+        const selector = `input[name=question${questionNumber}]:checked`;
         const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
+  
+        // if answer is correct
         if(userAnswer === currentQuestion.correctAnswer){
-            numCorrect++;
+          // add to the number of correct answers
+          numCorrect++;
+  
+          // color the answers green
+          answerContainers[questionNumber].style.color = 'lightgreen';
         }
-        answerContainers[questionNumbner].style.color = 'lightgreen';
-
+        // if answer is wrong or blank
         else{
-            answerContainers[questionNumber].style.color = 'red';
+          // color the answers red
+          answerContainers[questionNumber].style.color = 'red';
         }
-    });
-    resultsContainerContainer.innerHTML = `${numCorrect} out of ${myQuestions}.length`;
-}
-
-//Display the quiz
-
-buildQuiz();
-
-submitButton.addEventListener("click", showResults);
-
-const myQuestions = [
-    //First Question
-  {
-    question: "Which animal is a dog?",
-    answers: {
-      a: "Rock",
-      b: "Parrot",
-      c: "Table",
-    },
-
-    correctAnswer: "b",
-  },
-  //Second Question
-  {
-      question: "Which is a type of bird?",
-      answers: {
-          a: "Tucan",
-          b: "Siberian Husky",
-          c: "Mouse"
+      });
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+  
+    function showSlide(n) {
+      slides[currentSlide].classList.remove('active-slide');
+      slides[n].classList.add('active-slide');
+      currentSlide = n;
+      if(currentSlide === 0){
+        previousButton.style.display = 'none';
+      }
+      else{
+        previousButton.style.display = 'inline-block';
+      }
+      if(currentSlide === slides.length-1){
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+      }
+      else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+      }
+    }
+  
+    function showNextSlide() {
+      showSlide(currentSlide + 1);
+    }
+  
+    function showPreviousSlide() {
+      showSlide(currentSlide - 1);
+    }
+  
+    // Variables
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+      {
+        question: "Which one is an animal?",
+        answers: {
+          a: "Table",
+          b: "Bird",
+          c: "Rock"
+        },
+        correctAnswer: "b"
       },
-      correctAnswer: "a",
-  },
-  //Third Question
-  {
-  question: "How do birds teach their young to fly?",
-  answers: {
-    a: "Carry them on their backs",
-    b: "Slow take off from the ground",
-    c: "Yeet them out of the nest"
-  },
-  correctAnswer: "c",
-  }
-];
+      {
+        question: "Which one is a type of bird?",
+        answers: {
+          a: "Macaw",
+          b: "Beagle",
+          c: "Tabby"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "How do mother birds teach their babies to fly?",
+        answers: {
+          a: "Lovingly showing them how",
+          b: "Finding a human to do it",
+          c: "Yeeting them out of the nest",
+          d: "Black Magic"
+        },
+        correctAnswer: "c"
+      }
+    ];
+  
+    // Kick things off
+    buildQuiz();
+  
+    // Pagination
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+  
+    // Show the first slide
+    showSlide(currentSlide);
+  
+    // Event listeners
+    submitButton.addEventListener('click', showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+  })();
+  
